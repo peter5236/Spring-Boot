2bc682edf436a3330c857b997ed7009e335a3230
peterko5236 <peterko5236@gmail.com>
2022-04-11 09:50:37 +0900

    #6 스프링 DB접근 기술 - 순수 JDBC

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,24 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,7 +27,9 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
     //dependency injection 생성자 주입 권장
     //컴포넌트 스캔 사용 시 많은 부분의 변경이 필요할 수 있지만, 스프링 빈으로 등록하면 간단하게 변경 가능
