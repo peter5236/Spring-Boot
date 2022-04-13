2a781cf875ea74ba77d23d545407af2b448f7e60
peterko5236 <peterko5236@gmail.com>
2022-04-13 09:52:49 +0900

    #6 스프링 DB접근 기술 - 스프링 JdbcTemplate

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,6 +1,7 @@
 package hello.hellospring;
 
 import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
@@ -29,7 +30,9 @@ public class SpringConfig {
     public MemberRepository memberRepository() {
 
 //        return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+//        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
+
     }
     //dependency injection 생성자 주입 권장
     //컴포넌트 스캔 사용 시 많은 부분의 변경이 필요할 수 있지만, 스프링 빈으로 등록하면 간단하게 변경 가능
